fd294893c48807f9f428ed055afdf09e08483862
ladylovelacee <43499212+ladylovelacee@users.noreply.github.com>
2022-06-26 02:33:02 +0300

    stickman asset import

---
+++ Assets/Scripts/Runtime/Character/CharacterData.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterData", menuName = "Character", order = 1)]
+public class CharacterData : ScriptableObject
+{
+    public CharacterMovementData CharacterMovementData = new CharacterMovementData();    
+}
+
+[System.Serializable]
+public class CharacterMovementData
+{
+    public float ForwardSpeed = 6f;
+    public float SlideSpeed = 8f;
+}
---
+++ Assets/Scripts/Runtime/Character/CharacterPhysicsController.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterPhysicsController : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+    Rigidbody CharacterRb => (_rigidbody == null) ? _rigidbody = GetComponent<Rigidbody>() : _rigidbody;
+
+    public float sensitivityMultiplier = 0.01f;
+    public float deltaThreshold = .1f;
+    Vector2 firstTouchPosition;
+    float finalTouchX;
+    Vector2 curTouchPosition;
+
+    public float minXPos = -1.25f;
+    public float maxXPos = 1.25f;
+
+    private void Update()
+    {
+        HandleMovementWithSlide();
+    }
+
+    void HandleMovementWithSlide()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            firstTouchPosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            curTouchPosition = Input.mousePosition;
+            Vector2 touchDelta = (curTouchPosition - firstTouchPosition);
+
+            if (firstTouchPosition == curTouchPosition)
+            {
+                CharacterRb.velocity = new Vector3(0f, CharacterRb.velocity.y, CharacterRb.velocity.z);
+            }
+
+            //Debug.Log("firstTPos : " + firstTouchPosition + " - curTPos : " + curTouchPosition + " - touchDelta : " + touchDelta);
+            finalTouchX = transform.position.x;
+
+            if (Mathf.Abs(touchDelta.x) >= deltaThreshold)
+            {
+                finalTouchX = (transform.position.x + (touchDelta.x * sensitivityMultiplier));
+            }
+
+            CharacterRb.position = new Vector3(finalTouchX, transform.position.y, transform.position.z);
+            CharacterRb.position = new Vector3(Mathf.Clamp(CharacterRb.position.x, minXPos, maxXPos), CharacterRb.position.y, CharacterRb.position.z);
+
+            firstTouchPosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            ResetInputValues();
+        }
+
+    }
+
+    void ResetInputValues()
+    {
+        CharacterRb.velocity = new Vector3(0f, CharacterRb.velocity.y, CharacterRb.velocity.z);
+        firstTouchPosition = Vector2.zero;
+        finalTouchX = 0f;
+        curTouchPosition = Vector2.zero;
+    }
+}
